46706b6e1045b04e74ff22800a412ecb1542f095
daviP4iva <davi.pennamattos01@estudiant.upf.edu>
2025-05-09 16:37:34 +0200

    Solved MonteCarlo

--- lab2_T2G13/1_montecarlo/montecarlo.c
+++ lab2_T2G13/1_montecarlo/montecarlo.c
@@ -10,7 +10,7 @@ double pcg32_random(pcg32_random_t* rng)
 {
     uint64_t oldstate = rng->state;
     rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
-    uint32_t xorshifted = ((oldstate >> 18u) ˆ oldstate) >> 27u;
+    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
     uint32_t rot = oldstate >> 59u;
     uint32_t ran_int = (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
     return (double)ran_int / (double)UINT32_MAX;
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    int N = 3; //dimension of the circle
+    int N = 3;
     long NUM_SAMPLES = 1000000;
     long SEED = time(NULL);
 
@@ -42,6 +42,52 @@ int main(int argc, char *argv[])
 
     double ran = pcg32_random(&rng);
 
+    double ratio = 0;
+    double err = 0;
+    double elapsed_time = 0;
+    double start_time = MPI_Wtime();
+    for (int i = 0; i < NUM_SAMPLES; i++)
+    {
+        double x = pcg32_random(&ran);
+        double y = pcg32_random(&ran);
+        double z = pcg32_random(&ran);
+        double w = pcg32_random(&ran);
+        double d = 0;
+        
+        if (N >= 1) d += x * x;
+        if (N >= 2) d += y * y;
+        if (N >= 3) d += z * z;
+        if (N >= 4) d += w * w;
+        
+        if (d <= 1)
+        {
+            ratio++;
+        }
+    }
+    ratio /= NUM_SAMPLES;
+    err = sqrt(ratio * (1 - ratio) / NUM_SAMPLES);
+    elapsed_time = MPI_Wtime() - start_time;
+    
+    double total_ratio = 0.0;
+    double total_err = 0.0;
+    double total_time = 0.0;
+    
+    MPI_Reduce(&ratio, &total_ratio, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    MPI_Reduce(&err, &total_err, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    MPI_Reduce(&elapsed_time, &total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    
+    if (rank == 0) {
+        ratio = total_ratio / size;
+        err = sqrt(ratio * (1 - ratio) / NUM_SAMPLES);
+        elapsed_time = total_time;
+        
+        printf("Monte Carlo sphere/cube ratio estimation\n");
+        printf("N: %ld samples, d: %d, seed %ld, size: %d\n", NUM_SAMPLES, N, SEED, size);
+        printf("Ratio = %e (%e) Err: %e\n", ratio, ratio, err);
+        printf("Elapsed time: %f seconds\n", elapsed_time);
+    }
     MPI_Finalize();
     return 0;
 }
