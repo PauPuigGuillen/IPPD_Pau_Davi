97b63bbfe9567985b7da8cbf4d98f6362cc824ec
PauPuigGuillen <pau.puig04@estudiant.upf.edu>
2025-05-30 17:10:18 +0200

    job.sh for cuda vecadd (not tried)

--- lab3_T2G13/1_vecadd/CUDA/job.sh
+++ lab3_T2G13/1_vecadd/CUDA/job.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=vecadd_cuda
+#SBATCH --output=out_vecadd_cuda_%j.out
+#SBATCH --error=out_vecadd_cuda_%j.err
+#SBATCH --time=00:01:00
 #SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+
+make >> make.out || exit 1
+
+nvcc -o vecadd_cuda vecadd_cuda.cu 500
