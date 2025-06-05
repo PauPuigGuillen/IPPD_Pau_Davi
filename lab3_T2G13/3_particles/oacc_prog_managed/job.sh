08515715965bde00474733566e1e7a5ac3af2505
PauPuigGuillen <pau.puig04@estudiant.upf.edu>
2025-06-05 23:50:08 +0200

    makefile and job prepared for 3.2

---
+++ lab3_T2G13/3_particles/oacc_prog_managed/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=partis_oacc_prog_managed
+#SBATCH --output=out_partis_oacc_prog_managed_%j.out
+#SBATCH --error=out_partis_oacc_prog_managed_%j.err
+#SBATCH --time=00:01:00
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+
+make >> make.out || exit 1
+./partis_oacc_prog_managed 1000 1
